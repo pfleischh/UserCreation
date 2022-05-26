3b1063d5c478035648022ef28cec1567eec2a828
splootmctoot <pfleischh@gmail.com>
2022-05-25 21:03:45 -0500

    add validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  let errMsg = "";
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredPassword.trim().length === 0
+    ) {
+      return;
+    }
+    if(enteredPassword.trim().length < 8){
+      errMsg = "Password must be at least 8 characters.";
+      return;
+    }
+    setEnteredUsername("");
+    setEnteredPassword("");
+    console.log(enteredUsername, enteredPassword);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="password">Password</label>
-        <input id="password" type="text" />
+        <input
+          id="password"
+          type="text"
+          value={enteredPassword}
+          onChange={passwordChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
